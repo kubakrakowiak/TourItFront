e18ab3352c5e152a4d2c4453f5eefd257e26656a
michal.terlecki <michalt1204@gmail.com>
2023-11-15 02:02:16 +0100

    flexbile text

--- screens/authenticaded/WelcomePage.js
+++ screens/authenticaded/WelcomePage.js
@@ -51,6 +51,7 @@ const WelcomePage = () => {
               }}
               icon={"search"}
               textTransform="capitalize"
+              containerWidth="95%"
             />
           </View>
         </View>
@@ -104,12 +105,12 @@ const styles = StyleSheet.create({
     textAlign: "left",
     alignItems: "center",
     paddingLeft: "5%",
-    right:10
+    right:20
   },
   textSearchStyle:{
     color: "#323232",
     fontFamily: "SourceSansPro-Bold",
-    fontSize: '4vw',
+    fontSize: '5vw',
     fontStyle: "normal",
     fontWeight: "900",
     letterSpacing: 0.99,
