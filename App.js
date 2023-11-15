118d0b47410b83cbc6b134ecbec95b925eb90fa9
kubakrakowiak <jakubkrakowiak00@gmail.com>
2023-11-15 22:21:18 +0100

    fixed everything that wasnt working, dont touch

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import WelcomeScreen from "./screens/authenticaded/WelcomeScreen";
 import AppLoading from "expo-app-loading";
 import AuthContextProvider, { AuthContext } from "./store/auth-context";
 import { useContext } from "react";
-import {useFonts} from "expo-font";
+import { useFonts } from "expo-font";
 
 const Stack = createStackNavigator();
 
@@ -72,42 +72,39 @@ function Navigation() {
   );
 }
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Poppins-Black': require('./assets/fonts/Poppins-Black.ttf'),
-        'Poppins-BlackItalic': require('./assets/fonts/Poppins-BlackItalic.ttf'),
-        'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
-        'Poppins-BoldItalic': require('./assets/fonts/Poppins-BoldItalic.ttf'),
-        'Poppins-ExtraBold': require('./assets/fonts/Poppins-ExtraBold.ttf'),
-        'Poppins-ExtraBoldItalic': require('./assets/fonts/Poppins-ExtraBoldItalic.ttf'),
-        'Poppins-ExtraLight': require('./assets/fonts/Poppins-ExtraLight.ttf'),
-        'Poppins-ExtraLightItalic': require('./assets/fonts/Poppins-ExtraLightItalic.ttf'),
-        'Poppins-Italic': require('./assets/fonts/Poppins-Italic.ttf'),
-        'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
-        'Poppins-LightItalic': require('./assets/fonts/Poppins-LightItalic.ttf'),
-        'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
-        'Poppins-MediumItalic': require('./assets/fonts/Poppins-MediumItalic.ttf'),
-        'Poppins': require('./assets/fonts/Poppins-Regular.ttf'),
-        'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
-        'Poppins-SemiBoldItalic': require('./assets/fonts/Poppins-SemiBoldItalic.ttf'),
-        'Poppins-Thin': require('./assets/fonts/Poppins-Thin.ttf'),
-        'Poppins-ThinItalic': require('./assets/fonts/Poppins-ThinItalic.ttf'),
-    });
+  const [fontsLoaded] = useFonts({
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-BlackItalic": require("./assets/fonts/Poppins-BlackItalic.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-BoldItalic": require("./assets/fonts/Poppins-BoldItalic.ttf"),
+    "Poppins-ExtraBold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraBoldItalic": require("./assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    "Poppins-ExtraLight": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-ExtraLightItalic": require("./assets/fonts/Poppins-ExtraLightItalic.ttf"),
+    "Poppins-Italic": require("./assets/fonts/Poppins-Italic.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-LightItalic": require("./assets/fonts/Poppins-LightItalic.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-MediumItalic": require("./assets/fonts/Poppins-MediumItalic.ttf"),
+    Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-SemiBoldItalic": require("./assets/fonts/Poppins-SemiBoldItalic.ttf"),
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-ThinItalic": require("./assets/fonts/Poppins-ThinItalic.ttf"),
+  });
 
-    if (!fontsLoaded) {
-        return <AppLoading/>
-    }
-    return (
-        <SafeAreaView style={styles.container}>
-          <Navigation />
-        </SafeAreaView>
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navigation />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
