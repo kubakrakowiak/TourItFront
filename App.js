a142dca808fd15000ee4a80e32c863cba56520c8
kubakrakowiak <jakubkrakowiak00@gmail.com>
2023-11-15 20:44:00 +0100

    navigation to app js added

--- App.js
+++ App.js
@@ -1,10 +1,76 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./screens/auth/register/LoginScreen";
 import RegisterScreen from "./screens/auth/register/RegisterScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Colors } from "./constans/styles.js";
+import WelcomeScreen from "./screens/sample-auth/WelcomeScreen";
+
+const Stack = createStackNavigator();
+
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: "red" },
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          cardStyle: { backgroundColor: Colors.darkgreen },
+        }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{
+          cardStyle: { backgroundColor: Colors.darkgreen },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "red" },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: "red" },
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={WelcomeScreen}
+        options={{
+          cardStyle: { backgroundColor: Colors.darkgreen },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthenticatedStack />
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
-  return <RegisterScreen />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navigation />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
